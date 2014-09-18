e52a476f279ff7220b2d2baa5202ca5490c42225
akella <akella.a@gmail.com>
2014-09-18 17:37:54 +0300

    go

--- js/common.js
+++ js/common.js
@@ -3,6 +3,16 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-
+$('.menu__item').on('click',function(e){
+    e.preventDefault();
+    $(this).parent().siblings().children('.menu__item').removeClass('show').next().slideUp();
+    $(this).toggleClass('show').next().show();
+});
+$('.menu-i').hover(function() {
+	/* Stuff to do when the mouse enters the element */
+}, function() {
+	$(this).find('.submenu').hide("fast");
+});
 	console.log($('body').html());
 });
+
