3ab4cf0397e8f06ec1849a80c97c8dc4b30366ce
akella <akella.a@gmail.com>
2014-09-19 16:15:10 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -13,6 +13,34 @@ $('.menu-i').hover(function() {
 }, function() {
 	$(this).find('.submenu').hide("fast");
 });
+///menu-mob
+$('.mob__item span').on('click',function(e){
+    e.preventDefault();
+    $(this).parent().siblings().children('.mob__item span').removeClass('show').next().slideUp();
+    $(this).toggleClass('show').next().slideToggle();
+});
+///
 	console.log($('body').html());
 });
 
+
+$(document).ready(function(){
+	$(".open").click(function(){
+	$(".bb").slideToggle("normal"); return false;
+	});
+});
+$(function() {
+ var div = $(".bb").on("bb", function(e) {
+  div.slideDown() ;
+  e.stopPropagation() ;
+ }) ;
+ $(document).on("click", ".content", function() {
+  div.slideUp() ;
+ }) ;
+}) ;
+
+$(document).ready(function() {
+	$(".open").click(function() {
+		$(".open").toggleClass('act');
+	});
+});
